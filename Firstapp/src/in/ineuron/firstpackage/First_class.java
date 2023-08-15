8450ebf6e0c7453daa385f77df725c6d6d640b6e
rohith-515 <rgottipa@asu.edu>
2023-08-14 22:32:31 -0400

    First commit using eclipse

---
+++ Firstapp/src/in/ineuron/firstpackage/First_class.java
@@ -0,0 +1,10 @@
+package in.ineuron.firstpackage;
+
+public class First_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First classs in ineuron");
+	}
+
+}
