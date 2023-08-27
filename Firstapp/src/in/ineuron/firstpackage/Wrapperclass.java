72bf9a977f4d1fe3d2a809e14dec47280cf3ee27
rohith-515 <rgottipa@asu.edu>
2023-08-26 22:27:43 -0400

    wrapper classes example

---
+++ Firstapp/src/in/ineuron/firstpackage/Wrapperclass.java
@@ -0,0 +1,12 @@
+package in.ineuron.firstpackage;
+
+public class Wrapperclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i = new Integer(10);
+		System.out.println(i);
+
+	}
+
+}
