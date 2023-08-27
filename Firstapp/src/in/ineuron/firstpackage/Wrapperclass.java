f75dd87efec2058095e747521781d03250b117a4
rohith-515 <rgottipa@asu.edu>
2023-08-26 22:59:35 -0400

    changed wrapper

--- Firstapp/src/in/ineuron/firstpackage/Wrapperclass.java
+++ Firstapp/src/in/ineuron/firstpackage/Wrapperclass.java
@@ -4,8 +4,9 @@ public class Wrapperclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Integer i = new Integer(10);
-		System.out.println(i);
+		Integer i = new Integer("10");
+	
+		System.out.println(	i.byteValue());
 
 	}
 
