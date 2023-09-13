6a18e417048d4df9723f2e71c0117006169d9b48
rohith-515 <rgottipa@asu.edu>
2023-09-13 17:51:29 -0400

    pushing the code after generating the fourth token

--- Firstapp/src/in/ineuron/firstpackage/Wrapperclass.java
+++ Firstapp/src/in/ineuron/firstpackage/Wrapperclass.java
@@ -4,6 +4,7 @@ public class Wrapperclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		@SuppressWarnings("removal")
 		Integer i = new Integer("10");
 	
 		System.out.println(	i.byteValue());
